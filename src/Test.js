0bb46c4d0245824a48dc5512a7b695e9046ec674
syedadil0803 <129782085+Syedadil0803@users.noreply.github.com>
2026-03-24 08:44:04 +0530

    first commit

---
+++ src/Test.js
@@ -0,0 +1,12 @@
+import TestParagraph from "./TestP";
+
+
+function TestComponent(){
+    return(
+        <div>
+            <h1>Test Component</h1>
+            <TestParagraph />
+        </div>
+    )
+}
+export default TestComponent;
---
+++ src/TestP.js
@@ -0,0 +1,11 @@
+import './TestP.css';
+
+function TestParagraph(){
+    return(
+        <div>
+            <p className='p'>This is a test paragraph.</p>
+        </div>
+    )
+}
+
+export default TestParagraph;
